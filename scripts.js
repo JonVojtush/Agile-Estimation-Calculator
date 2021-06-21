961b1dfb58ba81af8a2fe53d736d33469c6d343f
JonVojtush <11096910+JonVojtush@users.noreply.github.com>
2021-06-21 09:45:37 -0400

    updated radio buttons

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var time
+    complexity
+    result
+
+document.getElementById("calculate").addEventListener("click", calculate);
+
+function calculate() {
+    alert ("Hello World!");
+}
+
+console.log();
