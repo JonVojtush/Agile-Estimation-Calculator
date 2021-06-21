fe066160b4a9466dd0623be5244381b83d05626c
JonVojtush <11096910+JonVojtush@users.noreply.github.com>
2021-06-21 10:52:56 -0400

    Modified layout primarily

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
-var time
-    complexity
-    result
+var value = 0
+document.getElementById("value").innerHTML = value;
 
-document.getElementById("calculate").addEventListener("click", calculate);
+document.getElementById("calculate").addEventListener("click", function calculate() {
+    document.getElementById("value").innerHTML = value+2; //+2 is TEMP to verify button functionality
+    console.log(value+2 + " Story Points"); //+2 is TEMP to verify button functionality
+    }
+);
 
-function calculate() {
-    alert ("Hello World!");
-}
-
-console.log();
+console.log(value + " Story Points");
